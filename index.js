b15f3ec4bc0499ec48716714c7aaf5db9247fecc
Skylar <skylarblessings5@gmail.com>
2023-03-14 01:12:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// index.js
+const main = document.querySelector("#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "John Doe is the champion";
+
+document.body.appendChild(newHeader);
+
+
+
+
